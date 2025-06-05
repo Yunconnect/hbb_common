e126d7fd906cea1f0842085fb0d839c65e4f0689
WHSHZTXZNB2020 <59988529+WHSHZTXZNB2020@users.noreply.github.com>
2025-06-05 10:45:27 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["xlyf.cc"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["115.120.225.176"];
 pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
